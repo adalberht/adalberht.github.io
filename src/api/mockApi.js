521efea2e3a18cef33a27864fc440a225139f8e8
albertusangga <albertusangga.98@gmail.com>
2017-12-02 04:23:33 +0700

    Add experiences section

--- src/api/mockApi.js
+++ src/api/mockApi.js
@@ -1,4 +1,6 @@
 // TO-DO: Make API
+import uiImageUrl from '../assets/makara-ui-fasilkom.jpg';
+import stoqoImageUrl from '../assets/stoqo.png';
 
 export const skills = {
   strong: [
@@ -19,27 +21,51 @@ export const experiences = {
   works: [
     {
       place: 'Jakarta, Indonesia',
-      time: 'Sep \'17 - Nov \'17',
+      time: "Sep '17 - Nov '17",
+      image: stoqoImageUrl,
       role: 'Part Time Software Engineer',
       company: 'Stoqo',
-      company_website: 'https://www.stoqo.com',
+      website: 'https://www.stoqo.com',
       description: 'Maintain and continue adding features to internal tool codebase. ',
-      tags: ['Python', 'Django', 'Django-REST', 'HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'styled-components'],
+      tags: [
+        'Python',
+        'Django',
+        'Django-REST',
+        'HTML',
+        'CSS',
+        'JavaScript',
+        'React',
+        'Redux',
+        'styled-components',
+      ],
     },
     {
       place: 'Jakarta, Indonesia',
-      time: 'Jun \'17 - Aug \'17',
+      time: "Jun '17 - Aug '17",
+      image: stoqoImageUrl,
       role: 'Software Engineer Intern',
       company: 'Stoqo',
-      company_website: 'https://www.stoqo.com',
-      description: '',
+      website: 'https://www.stoqo.com',
+      description: 'Developed internal tool, customer dashboard and merchant platform website',
       tags: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'styled-components', 'Django'],
+    },
+    {
+      place: 'Depok, Indonesia',
+      time: "Aug '17 - Dec '17",
+      image: uiImageUrl,
+      role: 'Assistant Lecturer',
+      company: 'Universitas Indonesia',
+      website: 'http://cs.ui. ac.id',
+      description:
+        'Tutors and grades CS Freshman student’s lab submissions & tasks for "Fundamentals of Programming I" course using Python programming language',
+      tags: ['Python'],
     },
   ],
   educations: [
     {
       place: 'Depok, Indonesia',
-      time: 'Aug \'16 - present',
+      time: "Aug '16 - present",
+      image: uiImageUrl,
       role: 'Computer Science, Bachelor Degree',
       institution: 'University of Indonesia',
       description: 'I studied Computer Science at one of the best university in Indonesia.',
--- src/containers/Experiences/Experiences.jsx
+++ src/containers/Experiences/Experiences.jsx
@@ -7,7 +7,7 @@ import { EXPERIENCES_ROUTE } from '../../constants/routes';
 import { loadExperiences } from '../../redux/modules/experiences';
 import Icon from '../../components/Icon';
 import LoadingIndicator from '../../components/LoadingIndicator';
-
+import About from '../About/index';
 
 @connect(state => ({ utils: state.utils, experiences: state.experiences }), { loadExperiences })
 class ExperiencesComponent extends Component {
@@ -45,29 +45,55 @@ class ExperiencesComponent extends Component {
           {!loading && (
             <Flex>
               <SectionTitle>Work Experiences</SectionTitle>
-              <ul>
+              <Timeline color={primaryColor}>
                 {works.map(work => (
-                  <li>
-                    <pre>
-                      <code>
-                        {JSON.stringify(work, 2, 2)}
-                      </code>
-                    </pre>
-                  </li>
+                  <TimelineEvent themeColor={themeColor}>
+                    <div className="time-and-place">
+                      <div className="place">{work.place}</div>
+                      <div className="time">{work.time}</div>
+                    </div>
+                    <div className="image">
+                      <img src={work.image} alt={work.place} />
+                    </div>
+                    <div className="panel">
+                      <div className="title">
+                        <span className="role">{work.role}</span>
+                        <span>-</span>
+                        <a href={work.website} className="institution">{work.company}</a>
+                      </div>
+                      <div className="description">{work.description}</div>
+                      <div className="tags">
+                        {work.tags.map(tag => <div className="tag">{tag}</div>)}
+                      </div>
+                    </div>
+                  </TimelineEvent>
                 ))}
-              </ul>
+              </Timeline>
               <SectionTitle>Educations</SectionTitle>
-              <ul>
+              <Timeline color={primaryColor}>
                 {educations.map(education => (
-                  <li>
-                    <pre>
-                      <code>
-                        {JSON.stringify(education, 2, 2)}
-                      </code>
-                    </pre>
-                  </li>
+                  <TimelineEvent themeColor={themeColor}>
+                    <div className="time-and-place">
+                      <div className="place">{education.place}</div>
+                      <div className="time">{education.time}</div>
+                    </div>
+                    <div className="image">
+                      <img src={education.image} alt={education.place} />
+                    </div>
+                    <div className="panel">
+                      <div className="title">
+                        <span className="role">{education.role}</span>
+                        <span>-</span>
+                        <a href={education.website} className="institution">{education.institution}</a>
+                      </div>
+                      <div className="description">{education.description}</div>
+                      <div className="tags">
+                        {education.tags.map(tag => <div className="tag">{tag}</div>)}
+                      </div>
+                    </div>
+                  </TimelineEvent>
                 ))}
-              </ul>
+              </Timeline>
             </Flex>
           )}
         </Container>
@@ -82,7 +108,7 @@ const Container = styled.div`
   align-items: center;
   color: ${props => props.primaryColor};
   display: flex;
-  font-family: ${props => props.theme.fonts.main};
+  font-family: ${props => props.theme.fonts.sans};
   flex-direction: column;
   min-height: 100vh;
   justify-content: center;
@@ -96,6 +122,9 @@ const Container = styled.div`
   }
   a {
     color: ${props => props.primaryColor};
+  }
+  .institution {
+    color: ${props => props.themeColor};
   }
 `;
 
@@ -111,9 +140,111 @@ Container.defaultProps = {
 const SectionTitle = styled.div`
   font-family: ${props => props.theme.fonts.sans};
   font-size: ${props => props.theme.textSizes['3xl']};
+  margin: ${props => props.theme.margin['4']};
 `;
 
 const Flex = styled.div`
   display: flex;
   flex-direction: column;
+`;
+
+const Timeline = styled.ul`
+  vertical-align: middle;
+  list-style: none;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  &::before {
+    display: initial;
+    bottom: 15rem;
+    content: '';
+    position: absolute;
+    top: 0;
+    left: calc(15% + 4.5rem);
+    width: 2px;
+    height: 75%;
+    background-color: ${props => props.color};
+  }
+  @media screen and (max-width: ${props => props.theme.screens.sm}) {
+    &::before {
+      display: none;
+    }
+    margin-left: 1rem;
+    margin-right: 1rem;
+    -webkit-margin-before: 0;
+    -webkit-margin-after: 0;
+  }
+`;
+
+const TimelineEvent = styled.li`
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: ${props => props.theme.margin['8']};
+  > * {
+    margin-right: ${props => props.theme.margin['4']};
+  }
+  img {
+    width: ${props => props.theme.width['12']};
+    height: ${props => props.theme.width['12']};
+    object-fit: contain;
+    border-radius: ${props => props.theme.borderRadius.full};
+  }
+  .time-and-place {
+    width: 15%;
+  }
+  .image {
+    position: relative;
+    min-height: 1px;
+    z-index: 1;
+  }
+  .image::after {
+    content: ' ';
+    display: block;
+    position: absolute;
+    top: -5px;
+    left: -5px;
+    height: calc(100% + 6px);
+    border-radius: 100%;
+    background: ${props => props.theme.colors.gray};
+    border: 2px solid #e4e4e4;
+    width: calc(100% + 6px);
+    z-index: 0;
+  }
+  .role {
+    font-weight: ${props => props.theme.fontWeights.bold};
+  }
+  .institution {
+  }
+  .panel {
+    width: 75%;
+    .title {
+      > * {
+        margin-right: ${props => props.theme.margin['2']};
+      }
+    }
+  }
+  .tags {
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+    .tag {
+      font-family: ${props => props.theme.fonts.mono};
+      padding: ${props => props.theme.padding['1']};
+      margin: ${props => props.theme.margin['1']};
+      margin-left: 0;
+      border-radius: ${props => props.theme.borderRadius.default};
+      border: 2px solid ${props => props.themeColor};
+    }
+  }
+  @media screen and (max-width: ${props => props.theme.screens.sm}) {
+    flex-direction: column;
+    .time-and-place {
+      width: 100%;
+      display: flex;
+      justify-content: space-between;
+    }
+    .image {
+      margin: ${props => props.theme.margin['2']};
+    }
+  }
 `;
